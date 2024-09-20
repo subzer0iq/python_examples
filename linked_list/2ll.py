88b643f9dcaa712fd08497b8a5551f70c122ac16
subzer0iq <belyak99@gmail.com>
2024-09-20 15:33:17 +0300

    .

--- linked_list/2ll.py
+++ linked_list/2ll.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 class Node():
-    def __init__(self, data):
+    def __init__(self, data, next=None, prev=None):
         self.data = data
-        self.next = None
-        self.prev = None
+        self.next = next
+        self.prev = prev
 
 class LinkedList:
     def __init__(self):
@@ -15,11 +15,23 @@ class LinkedList:
         new_node = Node(data)
         if not self.head:
             self.head = new_node
-            self.end= hew_node
-        #new_node.next = self.head
+            self.end = hew_node
         self.head.prev = new_node
         new_node.next = self.head
         self.head = new_node
+
+    def insert(self, after, data):
+        current_node = self.head
+        while current_node:
+            if current_node.data == after:
+                new_node = Node(data, current_node.next, current_node)
+                if current_node.next:
+                    current_node.next.prev = new_node
+                current_node.next = new_node
+                return
+            current_node = current_node.next
+        else:
+            print(f'{after} value not found')
 
     def append(self, data):
         new_node = Node(data)
@@ -36,10 +48,10 @@ class LinkedList:
         #current_node.next = new_node
 
     def show(self):
-        current_node = self.end
+        current_node = self.head
         while current_node:
-            print(current_node.data, end=' <=> ' if current_node.prev else '\n')
-            current_node = current_node.prev
+            print(current_node.data, end=' <=> ' if current_node.next else '\n')
+            current_node = current_node.next
 
 ll = LinkedList()
 ll.append(234)
@@ -47,4 +59,6 @@ ll.append(89)
 ll.append('sdgfg')
 ll.show()
 ll.prepend([123])
+ll.show()
+ll.insert(89,'aaaa')
 ll.show()
